626b0b603fe78157285da7ea395a4ff440d47d74
Sofia Agasieva <66107141+SofiaAgasieva@users.noreply.github.com>
2020-10-16 21:21:43 +0400

    Hello World

---
+++ Sofia-Hello-World.js
@@ -0,0 +1,18 @@
+var Sophia = { };
+
+Sophia.eat = () => {
+console.log("nom, nom, nom")
+};
+
+Sophia.poop = () => {
+console.log("wah, wah, wah");
+return("poop");
+}
+
+Sophia.sleep = () => {
+console.log("ZZZ");
+}
+
+Sophia.eat();
+Sophia.poop();
+Sophia.sleep();
